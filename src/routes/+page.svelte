7c4bbace48407a423540bddf40d861ae5ffa6e6a
Zhichen Guo <zg63@duke.edu>
2025-01-11 11:30:09 -0500

    tailwinds config fixed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-3xl font-bold underline">Flashcards Template</h1>
+<a href="/flashcards">flashcards</a>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
