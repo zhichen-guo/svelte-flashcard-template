a28c2e1aadfcb04c9e537a1f0f9080b8ce2b248a
Zhichen Guo <zg63@duke.edu>
2025-01-18 12:39:39 -0500

    progress bar

--- src/routes/flashcards/[slug]/test/+page.svelte
+++ src/routes/flashcards/[slug]/test/+page.svelte
@@ -1,7 +1,5 @@
 <script>
     import { fly, fade } from "svelte/transition";
-    import { Tween } from "svelte/motion";
-    import { cubicOut } from "svelte/easing";
     import Card from "$lib/components/card.svelte";
     const { data } = $props();
     const set_size = data.set.cards.length - 1;
@@ -14,14 +12,12 @@
         flip = false;
         translate_in = -200;
         card_index = (card_index == set_size) ? 0 : card_index + 1;
-        progress.target = card_index;
     }
 
     function backward() {
         flip = false;
         translate_in = 200;
         card_index = (card_index == 0) ? set_size : card_index - 1;
-        progress.target = card_index;
     }
 
     function onkeydown(event) {
@@ -37,38 +33,27 @@
                 break;
         }
     }    
-
-    let progress = new Tween(0, {
-        easing: cubicOut
-    });
 </script>
 
 <h1 class="text-3xl font-bold text-center pt-8 pb-10">{data.set.name}</h1>
 <div class="flex items-center justify-center py-8">
     <div>
-        <div class="relative h-60 w-96">
+        <div class="relative h-60 w-96 my-5">
             {#key card}
                 <div class="absolute top-0" onclick={() => {flip = !flip}} in:fly={{ x: translate_in }} out:fly={{ x: -translate_in }}>
                     <Card flip={flip} card={card} />
                 </div>
             {/key}
         </div>
-        
-        <div class="grid grid-cols-5 gap-3 pt-3">
-            <div class="text-center">
-                <p>{card_index} / {set_size}</p>
-            </div>
-            <div class="col-span-4">
-                <progress class="w-full h-3 rounded" max={set_size} value={progress.current}/>
-            </div>
 
-            <button onclick={backward} class="col-span-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <div class="grid grid-cols-3 gap-x-3">
+            <button onclick={backward} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 &lt;
             </button>
-            <button onclick={() => {flip = !flip}} class="col-span-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <button onclick={() => {flip = !flip}} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 { flip ? "show" : "hide" }
             </button>
-            <button onclick={forward} class="col-span-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <button onclick={forward} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 &gt;
             </button>
         </div>
